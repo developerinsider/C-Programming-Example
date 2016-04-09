ef0dd3e09cb2ef99c2af139551be27a4845b2c7e
Vineet Choudhary <Vineet Choudhary>
2016-04-09 16:46:10 +0530

    hello.c

---
+++ Simple Program/HELLO.C
@@ -0,0 +1,15 @@
+/*******************************************
+Statement - Print Hello World
+Programmer - Vineet Choudhary
+Written For - https://turboc.codeplex.com
+********************************************/
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	clrscr();
+	printf("Hello world\n");
+	getch();
+}
